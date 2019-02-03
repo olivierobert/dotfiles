82c3bf70fe512abe617f2be0ee5708a6ec005480
Olivier Robert <olivierobert@gmail.com>
2019-02-03 15:15:35 +0700

    Add NVM installation and initialization

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ source install/link.sh
 
 source install/git.sh
 
+
 # Perform macOS-specific install
 if [ "$(uname)" == "Darwin" ]; then
     echo -e "\\nRunning on macOS"
@@ -15,5 +16,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # Signin command disabled on macOS 10.13+: https://github.com/mas-cli/mas/issues/164
     # source install/mas.sh
 fi
+
+source install/nvm.sh
 
 echo "Done. Reload your terminal."
