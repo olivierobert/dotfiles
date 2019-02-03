5822b39c25f4dd623d0883ec1e5b4b16e5319bcc
Olivier Robert <olivierobert@gmail.com>
2019-02-03 15:16:26 +0700

    Add NVM installation script

---
+++ install/nvm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.zshrc.local" ]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\n" "$text" >> "$zshrc"
+    else
+      printf "\n%s\n" "$text" >> "$zshrc"
+    fi
+  fi
+}
+
+echo -e "\\nInstalling Node"
+echo "=============================="
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+if ! command -v nvm; then
+echo -e "\\Adding nvm to zshrc automatically..."
+  append_to_zshrc '# nvm path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion'
+fi
